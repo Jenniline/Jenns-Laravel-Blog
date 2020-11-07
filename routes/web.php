de50507db9d4dff5c2df63e4ea78c17c639a282c
Jenniline <jennilineebai@gmail.com>
2020-11-07 23:02:00 +0100

    Added the Slug in our URL Routes in Parts 24

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 
 
 Route::group(['middleware' => ['web']], function()  {
+    Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle']);
+
     Route::get('about', 'PagesController@getAbout');
 
     Route::get('contact', 'PagesController@getContact');
